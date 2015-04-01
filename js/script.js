aed814f7a005fbc6e1c477c1a8ab05a00370ca98
nonishenko <onishenko@ebola.com.ua>
2015-04-01 21:56:46 +0300

    12

--- js/script.js
+++ js/script.js
@@ -4,36 +4,55 @@ var AppNS = {};
 
     AppNS.init = function() {
         var position = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,''];
-        AppNS.mixItems(position);
-        AppNS.setPositions(position);
+        AppNS.newGame(position);
+        window.addEventListener('keydown', AppNS.keyboardEvent, false);
+        $('#new-game').on('click', function() { AppNS.newGame(position) });
 
-        $("body").on("click",".container__item",function(){
-            var pos = parseFloat($(this).data('name'));
+        $('.container').on('click', '.container__item', function(){
+            var $el = $(this);
+            var emptyPos = $('.empty').data('position');
+            var thisPos = $el.data('position');
+            var thisVal = $el.html();
+            moveRight(thisPos, emptyPos, thisVal, $el);
+            moveLeft(thisPos, emptyPos, thisVal, $el);
+            moveTop(thisPos, emptyPos, thisVal, $el);
+            moveBottom(thisPos, emptyPos, thisVal, $el);
 
-
-            if(core.check_top(pos)){
-                core.replace(pos, -64, 0, -4);
-            }
-
-            if(core.check_bottom(pos)){
-                core.replace(pos, 64, 0, 4);
-            }
-
-
-            if(core.check_right(pos)){
-                core.replace(pos, 0, 64, 1);
-            }
-
-
-            if(core.check_left(pos)){
-                core.replace(pos, 0, -64, -1);
-            }
-
-        });
-
-
+            AppNS.checkWinner();
+        })
     };
 
+    function moveRight(thisPos, emptyPos, thisVal, $el) {
+        if (thisPos != 4 && thisPos != 8 && thisPos != 12 && thisPos!= 16 ) {
+            if (emptyPos == (thisPos + 1)) {
+                clearCell(thisVal, $el)
+            }
+        }
+    }
+
+    function moveLeft(thisPos, emptyPos, thisVal, $el) {
+        if (thisPos != 1 && thisPos != 5 && thisPos != 9 && thisPos!= 13 ) {
+            if (emptyPos == (thisPos - 1)) {
+                clearCell(thisVal, $el)
+            }
+        }
+    }
+
+    function moveTop(thisPos, emptyPos, thisVal, $el) {
+        if (thisPos != 1 && thisPos != 2 && thisPos != 2 && thisPos!= 4 ) {
+            if (emptyPos == (thisPos - 4)) {
+                clearCell(thisVal, $el)
+            }
+        }
+    }
+
+    function moveBottom(thisPos, emptyPos, thisVal, $el) {
+        if (thisPos != 13 && thisPos != 14 && thisPos != 15 && thisPos!= 16 ) {
+            if (emptyPos == (thisPos + 4)) {
+                clearCell(thisVal, $el)
+            }
+        }
+    }
 
     AppNS.mixItems = function(position) {
         function random(min, max) {
@@ -50,38 +69,88 @@ var AppNS = {};
         return position;
     }
 
+    //function getXY(i) { return ' data-x='+( ((i-1) % 4)+1 )+' data-y='+Math.ceil( (i)/4); }
+
     AppNS.setPositions = function (position) {
-        var $container = $('.container');
-        var contentHtml = '';
-        console.log(position)
+        var $container = $('.container').empty();
+        var emptyCell;
         for (var i=0; i<position.length; i++) {
-            contentHtml = '<div class="container__item" data-position="'+(i+1)+'">'+position[i]+'</div>';
+            (position[i] == '') ? emptyCell = ' empty' : emptyCell = '';
+            var contentHtml = '<div class="container__item'+emptyCell+'" data-position="'+(i+1)+'">'+position[i]+'</div>';
+            //var contentHtml = '<div class="container__item'+emptyCell+'" '+getXY(i+1)+' data-position="'+(i+1)+'">'+position[i]+'</div>';
             $container.append(contentHtml)
         }
+
+    }
+
+    AppNS.newGame = function(position) {
+        AppNS.mixItems(position);
+        AppNS.setPositions(position);
+    }
+
+    //AppNS.keyboardEvent = function(e) {
+    //    switch(e.keyCode) {
+    //        case 38: AppNS.key('up'); break;
+    //        case 40: AppNS.key('down'); break;
+    //        case 37: AppNS.key('left'); break;
+    //        case 39: AppNS.key('right'); break;
+    //    }
+    //}
+    function clearCell(thisVal, $el) {
+        $('.empty').removeClass('empty').html(thisVal);
+        $el.addClass('empty').empty();
+    }
+
+
+    //AppNS.key = function ( type ) {
+    //    var $emptyEl = $('.empty');
+    //    var emptyPos = $emptyEl.data('position');
+    //    switch (type) {
+    //        case 'up':
+    //            console.log(emptyPos);
+    //            if (emptyPos != 1 && emptyPos != 2 && emptyPos != 2 && emptyPos!= 4 ) {
+    //                var thisVal = $('.container__item').data('position', emptyPos+4).html();
+    //                clearCell(thisVal, $emptyEl)
+    //            }
+    //
+    //            //var from = 'x' + a + 'y' + (b + 1);
+    //            //var to = 'x' + a + 'y' + b;
+    //            break;
+    //        case 'down':
+    //            var from = 'x' + a + 'y' + (4 - b);
+    //            var to = 'x' + a + 'y' + (5 - b);
+    //            break;
+    //        case 'left':
+    //            var from = 'x' + (b + 1) + 'y' + a;
+    //            var to = 'x' + b + 'y' + a;
+    //            break;
+    //        case 'right':
+    //            var from = 'x' + (4 - b) + 'y' + a;
+    //            var to = 'x' + (5 - b) + 'y' + a;
+    //            break;
+    //    }
+    //    if( !$('.'+to).length ) {
+    //    //    $('.empty').css('top', '100px')
+    //        $('.'+from).removeClass(from).addClass(to);
+    //        AppNS.checkWinner();
+    //        return
+    //    }
+    //
+    //}
+
+    AppNS.checkWinner = function() {
+        var correctCell = 0;
+        $('.container__item').not(".empty").each(function(){
+            var el = $(this);
+            if (el.data('position') == el.html()) {
+                correctCell ++;
+            }
+        })
+        console.log(correctCell)
+        if (correctCell == 15) alert('You won!');
     }
 
 
     $(AppNS.init);
 
 })(jQuery);
-
-
-var core = {
-    replace : function(position, func_top, func_left, func_position){
-
-        var obj = $('#'+position);
-        var left = obj.offset().left;
-        var top = obj.offset().top;
-
-        new_position = new Object();
-        new_position.top = top + func_top;
-        new_position.left = left + func_left;
-        core.table_of_emptify[position] = true;
-        core.table_of_emptify[position+func_position] = false;
-        obj.attr("id",(position+func_position));
-        new_position.left = left + func_left;
-        obj.offset(new_position);
-        core.check_pos(position+func_position);
-
-    },
-}
